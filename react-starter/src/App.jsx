bd24b6204284143ac487a961e93cff780d3d8e94
kabeerqadirsiba <kabeerqadirsiba@gmail.com>
2025-03-22 14:11:05 +0500

    login functionlaity added

--- react-starter/src/App.jsx
+++ react-starter/src/App.jsx
@@ -1,13 +1,34 @@
 import Button from "./Button"
 import Dashboard from "./Dashboard"
 import Component from "./Component"
+import { useState } from "react"
 // import React from "react"
 function App(){
-  const user = {
-    name:'kaleem',
-    cast:'shar',
-    isLoggedin:false
+const [username, setUsername] = useState("")
+const [passsword, setPassword] = useState("")
+const [department, setDapartment] = useState("")
+
+const handleSubmit = (event) =>{
+  event.preventDefault()
+  if(passsword.length < 5){
+    alert("kindly set password more than 5 characters")
+    setUsername("")
+    setPassword("")
+    setDapartment("")
   }
+  else{
+    if(passsword === 'hassan'){
+      console.log("you are authenticated user ")
+      setUsername("")
+      setPassword("")
+      setDapartment("")
+    }
+    else{
+      alert("wrong credientials")
+    }
+  }
+
+}
   return(
 <>
 {/* <Button color={'yellow'} handlerFunction={()=> alert("clicked")} name={'Login'} /> */}
@@ -17,14 +38,25 @@ function App(){
 
 <Button color={'white'} handlerFunction={()=> alert("clicked")} name={'Login'} /> */}
 
-{user && <h1>user found</h1>}
+{/* {user && <h1>user found</h1>} */}
 
 {/* {user.isLoggedin? (
 <Dashboard username={user.name}/>
 ):(
 <h1>login first</h1>
 )} */}
-<Component/>
+{/* <Component/> */}
+
+
+<div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+
+<form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", width: "300px", gap: "10px"}}>
+<input type="text" value={username} onChange={(event)=>setUsername(event.target.value)} placeholder="username" />
+<input type="password" value={passsword}  onChange={(event)=>setPassword(event.target.value)} placeholder="password" />
+<input type="text" value={department}  onChange={(event)=>setDapartment(event.target.value)} placeholder="department" />
+<button type="submit">Login</button>
+</form>
+</div>
 </>
   )
 }
